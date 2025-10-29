8613791e4f202dc7b1cc8ac0f444d266e98f4786
98prabowo <dimasprabowo98@icloud.com>
2025-10-30 02:26:00 +0700

    feat(instructions): create instructions enum

---
+++ src/instructions/mod.rs
@@ -0,0 +1,16 @@
+mod burn_and_refund_v1;
+mod init_config_v1;
+mod mint_and_vault_v1;
+
+pub use burn_and_refund_v1::*;
+pub use init_config_v1::*;
+pub use mint_and_vault_v1::*;
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
+pub enum Instructions {
+    InitConfigV1(InitConfigV1InstructionData),
+    MintAndVaultV1(MintAndVaultV1InstructionData),
+    BurnAndRefundV1,
+}
