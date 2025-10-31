dff71aab5be74744ac427cdea380692b6ffa40c6
98prabowo <dimasprabowo98@icloud.com>
2025-11-01 03:55:28 +0700

    fix(instructions): update instructions module

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,16 +1,19 @@
 mod burn_and_refund_v1;
 mod init_config_v1;
 mod mint_and_vault_v1;
+mod update_nft_v1;
 
 pub use burn_and_refund_v1::*;
 pub use init_config_v1::*;
 pub use mint_and_vault_v1::*;
+pub use update_nft_v1::*;
 
 use borsh::{BorshDeserialize, BorshSerialize};
 
 #[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
-pub enum Instructions {
+pub enum TweetoniumInstruction {
     InitConfigV1(InitConfigV1InstructionData),
     MintAndVaultV1(MintAndVaultV1InstructionData),
+    UpdateNftV1(UpdateNftV1InstructionData),
     BurnAndRefundV1,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-#![cfg_attr(not(check_cfg), allow(unexpected_cfgs))]
-#![cfg(check_cfg)]
-#![check_cfg(
-    feature = "custom-heap",
-    feature = "custom-panic",
-    target_os = "solana"
-)]
-
 use borsh::BorshDeserialize;
 use solana_program::{
     account_info::AccountInfo, declare_id, entrypoint, entrypoint::ProgramResult, msg,
@@ -13,7 +5,9 @@ use solana_program::{
 };
 
 use crate::{
-    instructions::{BurnAndRefundV1, InitConfigV1, Instructions, MintAndVaultV1},
+    instructions::{
+        BurnAndRefundV1, InitConfigV1, MintAndVaultV1, TweetoniumInstruction, UpdateNftV1,
+    },
     utils::ProcessInstruction,
 };
 
@@ -30,19 +24,23 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let instruction = Instructions::try_from_slice(instruction_data)
+    let instruction = TweetoniumInstruction::try_from_slice(instruction_data)
         .map_err(|_| ProgramError::InvalidInstructionData)?;
 
     match instruction {
-        Instructions::InitConfigV1(data) => {
+        TweetoniumInstruction::InitConfigV1(data) => {
             msg!("Instruction: InitializeConfig");
             InitConfigV1::try_from((accounts, data, program_id))?.process()
         }
-        Instructions::MintAndVaultV1(data) => {
+        TweetoniumInstruction::MintAndVaultV1(data) => {
             msg!("Instruction: MintAndVault");
             MintAndVaultV1::try_from((accounts, data, program_id))?.process()
         }
-        Instructions::BurnAndRefundV1 => {
+        TweetoniumInstruction::UpdateNftV1(data) => {
+            msg!("Instruction: UpdateNft");
+            UpdateNftV1::try_from((accounts, data))?.process()
+        }
+        TweetoniumInstruction::BurnAndRefundV1 => {
             msg!("Instruction: BurnAndRefund");
             BurnAndRefundV1::try_from((accounts, program_id))?.process()
         }
