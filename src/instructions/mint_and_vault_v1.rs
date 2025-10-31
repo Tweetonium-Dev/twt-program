e19d3571194b373e4086c62dac6a7cc617209869
98prabowo <dimasprabowo98@icloud.com>
2025-11-01 03:54:45 +0700

    feat(instructions): update mint and store to vault

--- src/instructions/mint_and_vault_v1.rs
+++ src/instructions/mint_and_vault_v1.rs
@@ -1,28 +1,37 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    account_info::AccountInfo, entrypoint::ProgramResult, msg, program::{invoke, invoke_signed}, program_error::ProgramError, pubkey::Pubkey, rent::Rent, sysvar::Sysvar
+use mpl_core::{
+    instructions::CreateV1CpiBuilder,
 };
-use solana_system_interface::instruction as system_instruction;
-use spl_token_interface::instruction as token_instruction;
+use solana_program::{
+    account_info::AccountInfo, entrypoint::ProgramResult, msg, program_error::ProgramError,
+    pubkey::Pubkey,
+};
 
 use crate::{
-    states::{Config, MintedUser, Vault, MINTED_USER_SEED, VAULT_SEED},
-    utils::{sha256_hash, verify_merkle_proof, AccountCheck, ProcessInstruction, SignerAccount, SystemAccount, WritableAccount},
+    states::{Config, MINTED_USER_SEED, MintedUser, VAULT_SEED, Vault},
+    utils::{
+        AccountCheck, AccountUninitializedCheck, AssociatedTokenAccount,
+        AssociatedTokenAccountCheck, ConfigAccount, MintAccount, MplCoreAccount, Pda,
+        ProcessInstruction, SignerAccount, SystemAccount, TokenProgram, TransferArgs,
+        WritableAccount, sha256_hash, verify_merkle_proof,
+    },
 };
 
 #[derive(Debug)]
 pub struct MintAndVaultV1Accounts<'a, 'info> {
-    pub authority: &'a AccountInfo<'info>,          // config authority (not required signer for mint)
-    pub payer: &'a AccountInfo<'info>,              // user wallet paying price
-    pub config_pda: &'a AccountInfo<'info>,         // config PDA
-    pub vault_pda: &'a AccountInfo<'info>,          // vault data PDA (holds Vault struct)
-    pub vault_ata: &'a AccountInfo<'info>,          // token account owned by vault authority (ATA)
-    pub payer_ata: &'a AccountInfo<'info>,          // payer's ZDLT token account (ATA)
-    pub minted_user: &'a AccountInfo<'info>,        // usermint PDA (per-wallet minted flag)
-    pub nft_mint: &'a AccountInfo<'info>,           // NFT mint
-    pub nft_token_account: &'a AccountInfo<'info>,  // user's NFT token account (where minted token will be sent)
-    pub token_program: &'a AccountInfo<'info>,      // SPL token program
-    pub system_program: &'a AccountInfo<'info>,     // System program
+    pub authority: &'a AccountInfo<'info>, // Config authority (not required signer for mint)
+    pub payer: &'a AccountInfo<'info>,     // User wallet paying price
+    pub config_pda: &'a AccountInfo<'info>, // Config PDA
+    pub vault_pda: &'a AccountInfo<'info>, // Vault data PDA (holds Vault struct)
+    pub vault_ata: &'a AccountInfo<'info>, // Token account owned by vault authority (ATA)
+    pub payer_ata: &'a AccountInfo<'info>, // Payer's associated token account
+    pub minted_user_pda: &'a AccountInfo<'info>, // MintedUser PDA (per-wallet minted flag)
+    pub nft_asset: &'a AccountInfo<'info>, // NFT mint
+    pub nft_token_account: &'a AccountInfo<'info>, // User's NFT token account (where minted token will be sent)
+    pub token_mint: &'a AccountInfo<'info>,        // Token mint will be stored to vault (e.g. ZDLT)
+    pub token_program: &'a AccountInfo<'info>,     // SPL token program
+    pub system_program: &'a AccountInfo<'info>,    // System program
+    pub mpl_core: &'a AccountInfo<'info>,          // Metaplex core program
 }
 
 impl<'a, 'info> TryFrom<&'a [AccountInfo<'info>]> for MintAndVaultV1Accounts<'a, 'info> {
@@ -30,30 +39,40 @@ impl<'a, 'info> TryFrom<&'a [AccountInfo<'info>]> for MintAndVaultV1Accounts<'a,
 
     fn try_from(accounts: &'a [AccountInfo<'info>]) -> Result<Self, Self::Error> {
         let [
-        authority, 
-        payer, 
-        config_pda, 
-        vault_pda, 
-        vault_ata, 
-        payer_ata, 
-        minted_user, 
-        nft_mint, 
-        nft_token_account, 
-        token_program, 
-        system_program
-        ] = accounts else {
+            authority,
+            payer,
+            config_pda,
+            vault_pda,
+            vault_ata,
+            payer_ata,
+            minted_user_pda,
+            nft_asset,
+            nft_token_account,
+            token_mint,
+            token_program,
+            system_program,
+            mpl_core,
+        ] = accounts
+        else {
             return Err(ProgramError::NotEnoughAccountKeys);
         };
 
         SignerAccount::check(payer)?;
         WritableAccount::check(config_pda)?;
+        ConfigAccount::check(config_pda)?;
         WritableAccount::check(vault_pda)?;
         WritableAccount::check(vault_ata)?;
+        AssociatedTokenAccount::check(vault_ata, vault_pda.key, token_mint.key, token_program.key)?;
         WritableAccount::check(payer_ata)?;
-        WritableAccount::check(minted_user)?;
-        WritableAccount::check(nft_mint)?;
+        AssociatedTokenAccount::check(payer_ata, payer.key, token_mint.key, token_program.key)?;
+        WritableAccount::check(minted_user_pda)?;
+        WritableAccount::check(nft_asset)?;
+        WritableAccount::check_uninitialized(nft_asset)?;
         WritableAccount::check(nft_token_account)?;
+        AssociatedTokenAccount::check(nft_token_account, payer.key, nft_asset.key, token_program.key)?;
+        MintAccount::check(token_mint)?;
         SystemAccount::check(system_program)?;
+        MplCoreAccount::check(mpl_core)?;
 
         Ok(Self {
             authority,
@@ -62,11 +81,13 @@ impl<'a, 'info> TryFrom<&'a [AccountInfo<'info>]> for MintAndVaultV1Accounts<'a,
             vault_ata,
             config_pda,
             vault_pda,
-            minted_user,
-            nft_mint,
+            minted_user_pda,
+            nft_asset,
             nft_token_account,
+            token_mint,
             token_program,
             system_program,
+            mpl_core,
         })
     }
 }
@@ -74,6 +95,8 @@ impl<'a, 'info> TryFrom<&'a [AccountInfo<'info>]> for MintAndVaultV1Accounts<'a,
 #[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
 pub struct MintAndVaultV1InstructionData {
     pub merkle_proof: Vec<[u8; 32]>,
+    pub name: String,
+    pub uri: String,
 }
 
 #[derive(Debug)]
@@ -81,6 +104,132 @@ pub struct MintAndVaultV1<'a, 'info> {
     pub accounts: MintAndVaultV1Accounts<'a, 'info>,
     pub instruction_data: MintAndVaultV1InstructionData,
     pub program_id: &'a Pubkey,
+}
+
+impl<'a, 'info> MintAndVaultV1<'a, 'info> {
+    fn check_mint_eligibility(&self, config: &Config) -> ProgramResult {
+        let payer = self.accounts.payer;
+        let authority = self.accounts.authority;
+
+        if config.authority != *authority.key {
+            msg!("Authority account mismatch with config");
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        if config.supply_minted >= config.max_supply {
+            msg!("Sold out");
+            return Err(ProgramError::Custom(0));
+        }
+
+        if config.supply_minted < config.released {
+            let leaf = sha256_hash(&payer.key.to_bytes());
+            if !verify_merkle_proof(
+                leaf,
+                &self.instruction_data.merkle_proof,
+                config.merkle_root.to_bytes(),
+            ) {
+                msg!("Not whitelisted");
+                return Err(ProgramError::Custom(1));
+            }
+        }
+
+        Ok(())
+    }
+
+    fn init_minted_user(&self) -> Result<(), ProgramError> {
+        let payer = self.accounts.payer;
+        let minted_user_pda = self.accounts.minted_user_pda;
+        let system_program = self.accounts.system_program;
+
+        Pda::new(
+            payer,
+            minted_user_pda,
+            system_program,
+            &[MINTED_USER_SEED, payer.key.as_ref()],
+            MintedUser::LEN,
+            self.program_id,
+            self.program_id,
+        )?
+        .init_if_needed()?;
+
+        let minted = MintedUser::new(*payer.key, false);
+        let minted_user_data = &mut minted_user_pda.data.borrow_mut()[..MintedUser::LEN];
+        MintedUser::init(minted_user_data, &minted)?;
+
+        Ok(())
+    }
+
+    fn transfer_to_vault(
+        &self,
+        config: &mut Config,
+        minted_user: &mut MintedUser,
+    ) -> ProgramResult {
+        let payer = self.accounts.payer;
+        let payer_ata = self.accounts.payer_ata;
+        let vault_ata = self.accounts.vault_ata;
+        let config_pda = self.accounts.config_pda;
+        let vault_pda = self.accounts.vault_pda;
+        let nft_mint = self.accounts.nft_asset;
+        let token_mint = self.accounts.token_mint;
+        let token_program = self.accounts.token_program;
+        let system_program = self.accounts.system_program;
+
+        let price = config.price;
+
+        TokenProgram::transfer(TransferArgs {
+            source: payer_ata,
+            destination: vault_ata,
+            authority: payer,
+            mint: token_mint,
+            token_program,
+            signer_pubkeys: &[],
+            amount: price,
+            decimals: config.mint_decimals,
+        })?;
+
+        let vault_bump = Pda::new(
+            payer,
+            vault_pda,
+            system_program,
+            &[VAULT_SEED, config_pda.key.as_ref()],
+            Vault::LEN,
+            self.program_id,
+            self.program_id,
+        )?
+        .init_if_needed()?;
+
+        let vault = Vault::new(*payer.key, *nft_mint.key, price, false, [vault_bump]);
+        let vault_data = &mut vault_pda.data.borrow_mut()[..Vault::LEN];
+        Vault::init(vault_data, &vault)?;
+
+        minted_user.set_minted(true);
+        config.increment_minted()?;
+
+        Ok(())
+    }
+
+    fn mint_nft(self) -> ProgramResult {
+        let payer = self.accounts.payer;
+        let authority = self.accounts.authority;
+        let nft_asset = self.accounts.nft_asset;
+        let nft_token_account = self.accounts.nft_token_account;
+        let system_program = self.accounts.system_program;
+        let mpl_core = self.accounts.mpl_core;
+
+        CreateV1CpiBuilder::new(mpl_core)
+            .asset(nft_asset)
+            .collection(None)
+            .authority(Some(authority))
+            .payer(payer)
+            .owner(Some(nft_token_account))
+            .update_authority(Some(authority))
+            .system_program(system_program)
+            .name(self.instruction_data.name)
+            .uri(self.instruction_data.uri)
+            .invoke()?;
+
+        Ok(())
+    }
 }
 
 impl<'a, 'info>
@@ -111,150 +260,29 @@ impl<'a, 'info>
 
 impl<'a, 'info> ProcessInstruction for MintAndVaultV1<'a, 'info> {
     fn process(self) -> ProgramResult {
-        let payer = self.accounts.payer;
-        let payer_ata = self.accounts.payer_ata;
-        let vault_ata = self.accounts.vault_ata;
-        let config_pda = self.accounts.config_pda;
-        let vault_pda = self.accounts.vault_pda;
-        let minted_user = self.accounts.minted_user;
-        let nft_mint = self.accounts.nft_mint;
-        let token_program = self.accounts.token_program;
-        let system_program = self.accounts.system_program;
-        let authority = self.accounts.authority;
+        let mut config_data = self.accounts.config_pda.data.borrow_mut();
+        let mut config = Config::load_mut(&mut config_data)?;
 
-        // Check mint eligibility from config
-        let mut cfg = Config::load(&config_pda.data.borrow())?;
+        self.check_mint_eligibility(&config)?;
 
-        if cfg.authority != *authority.key {
-            msg!("Authority account mismatch with config");
-            return Err(ProgramError::InvalidAccountData);
-        }
-
-        if cfg.supply_minted >= cfg.max_supply {
-            msg!("Sold out");
-            return Err(ProgramError::Custom(0));
-        }
-
-        if cfg.supply_minted < cfg.released {
-            let leaf = sha256_hash(&payer.key.to_bytes());
-            if !verify_merkle_proof(leaf, &self.instruction_data.merkle_proof, cfg.merkle_root.to_bytes()) {
-                msg!("Not whitelisted");
-                return Err(ProgramError::Custom(1));
-            }
-        }
-
-        // Minted user PDA
-        let (expected_minted_user_pda, minted_user_bump) = Pubkey::find_program_address(
-            &[MINTED_USER_SEED, payer.key.as_ref()], 
-            self.program_id
-        );
-        if expected_minted_user_pda != *minted_user.key {
-            msg!("MintedUser PDA mismatch");
-            return Err(ProgramError::InvalidAccountData);
-        }
-
-        // Create minted user PDA if needed
-        if minted_user.lamports() == 0 ||minted_user.data_is_empty() {
-            let rent = Rent::get()?;
-            let space = MintedUser::LEN;
-            let lamports = rent.minimum_balance(space);
-
-            invoke_signed(
-                &system_instruction::create_account(
-                    payer.key,
-                    minted_user.key,
-                    lamports,
-                    space as u64,
-                    self.program_id,
-                ),
-                &[payer.clone(), minted_user.clone(), system_program.clone()],
-                &[&[MINTED_USER_SEED, payer.key.as_ref(), &[minted_user_bump]]],
-            )?;
-
-            let mu = MintedUser {
-                owner: *payer.key,
-                minted: false,
-            };
-            mu.serialize(&mut &mut minted_user.data.borrow_mut()[..])?;
-        }
+        self.init_minted_user()?;
 
         // Read minted user
-        let mut usermint = MintedUser::load(&minted_user.data.borrow())?;
-        if usermint.minted {
+        let mut minted_user_data = self.accounts.minted_user_pda.data.borrow_mut();
+        let mut minted_user = MintedUser::load_mut(&mut minted_user_data)?;
+        if minted_user.is_minted() {
             msg!("Already minted");
             return Err(ProgramError::Custom(2));
         }
 
-        // Transfer token (e.g: ZDLT) from payer -> vault
-        let price = cfg.price;
-        let transfer_to_vault_ix = token_instruction::transfer(
-            token_program.key,
-            payer_ata.key,
-            vault_ata.key,
-            payer.key,
-            &[],
-           price 
-        )?;
-        invoke(
-            &transfer_to_vault_ix,
-            &[
-                payer_ata.clone(),
-                vault_ata.clone(),
-                payer.clone(),
-                token_program.clone(),
-            ]
-        )?;
+        self.transfer_to_vault(&mut config, &mut minted_user)?;
 
-        // Vault PDA
-        let (expected_vault_pda, vault_bump) = Pubkey::find_program_address(
-            &[VAULT_SEED, payer.key.as_ref()], 
-            self.program_id
-        );
-        if expected_vault_pda != *vault_pda.key {
-            msg!("Vault PDA mismatch");
-            return Err(ProgramError::InvalidAccountData);
-        }
+        self.mint_nft()?;
 
-        // Create vault PDA if needed
-        if vault_pda.lamports() == 0 || vault_pda.data_is_empty() {
-            let rent = Rent::get()?;
-            let space = Vault::LEN;
-            let lamports = rent.minimum_balance(space);
-
-            invoke_signed(
-                &system_instruction::create_account(
-                    payer.key,
-                    vault_pda.key,
-                    lamports,
-                    space as u64,
-                    self.program_id,
-                ),
-                &[payer.clone(), vault_pda.clone(), system_program.clone()],
-                &[&[VAULT_SEED, config_pda.key.as_ref(), &[vault_bump]]],
-            )?;
-        }
-
-        // Write vault record
-        let v = Vault {
-            owner: *payer.key,
-            nft: *nft_mint.key,
-            amount: price,
-            is_unlocked: false,
-            bump: [vault_bump],
-        };
-        v.serialize(&mut &mut vault_pda.data.borrow_mut()[..])?;
-
-        // Mark minted user as minted
-        usermint.minted = true;
-        usermint.serialize(&mut &mut minted_user.data.borrow_mut()[..])?;
-
-        // Increment supply minted
-        cfg.supply_minted = cfg.supply_minted.checked_add(1).ok_or(ProgramError::Custom(3))?;
-        cfg.serialize(&mut &mut config_pda.data.borrow_mut()[..])?;
-
-        // TODO: Mint NFT with mpl-core
+        let price = config.price;
 
         msg!("MintAndVault: minted NFT and escrowed {} tokens", price);
+
         Ok(())
     }
 }
