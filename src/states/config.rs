f86c66246d53e78f9e9b9a8399b2f0a79b1ce111
98prabowo <dimasprabowo98@icloud.com>
2025-11-01 03:53:38 +0700

    feat(states): add bytemuck load to config

--- src/states/config.rs
+++ src/states/config.rs
@@ -1,9 +1,10 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{borsh1::try_from_slice_unchecked, program_error::ProgramError, pubkey::Pubkey};
+use bytemuck::{Pod, Zeroable};
+use solana_program::{program_error::ProgramError, pubkey::Pubkey};
 
 pub const CONFIG_SEED: &[u8; 6] = b"config";
 
-#[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
+#[repr(C, packed)]
+#[derive(Debug, Clone, Copy, Pod, Zeroable)]
 pub struct Config {
     pub authority: Pubkey,
     pub max_supply: u64,
@@ -13,6 +14,7 @@ pub struct Config {
     pub vesting_end_ts: i64,
     pub merkle_root: Pubkey,
     pub mint: Pubkey,
+    pub mint_decimals: u8,
 }
 
 impl Config {
@@ -23,11 +25,48 @@ impl Config {
         + size_of::<u64>()
         + size_of::<i64>()
         + size_of::<Pubkey>()
-        + size_of::<Pubkey>();
+        + size_of::<Pubkey>()
+        + size_of::<u8>();
 
     #[inline(always)]
-    pub fn load(data: &[u8]) -> Result<Self, ProgramError> {
-        let result: Self = try_from_slice_unchecked(data)?;
-        Ok(result)
+    pub fn load(data: &[u8]) -> Result<&Self, ProgramError> {
+        if data.len() < Self::LEN {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let config: &Self = bytemuck::try_from_bytes(&data[..Self::LEN])
+            .map_err(|_| ProgramError::InvalidAccountData)?;
+
+        Ok(config)
+    }
+
+    #[inline(always)]
+    pub fn load_mut(data: &mut [u8]) -> Result<&mut Self, ProgramError> {
+        if data.len() < Self::LEN {
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let config: &mut Self = bytemuck::try_from_bytes_mut(&mut data[..Self::LEN])
+            .map_err(|_| ProgramError::InvalidAccountData)?;
+
+        Ok(config)
+    }
+
+    #[inline(always)]
+    pub fn init(data: &mut [u8], cfg: &Self) -> Result<(), ProgramError> {
+        if data.len() < Self::LEN {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        data.copy_from_slice(bytemuck::bytes_of(cfg));
+        Ok(())
+    }
+
+    #[inline(always)]
+    pub fn increment_minted(&mut self) -> Result<(), ProgramError> {
+        self.supply_minted = self
+            .supply_minted
+            .checked_add(1)
+            .ok_or(ProgramError::InvalidInstructionData)?;
+        Ok(())
     }
 }
