5949b471dd41a9954179f9d841f08a07f270cc01
98prabowo <dimasprabowo98@icloud.com>
2025-11-01 03:54:58 +0700

    feat(instructions): update burn and refund

--- src/instructions/burn_and_refund_v1.rs
+++ src/instructions/burn_and_refund_v1.rs
@@ -1,19 +1,13 @@
-use borsh::BorshSerialize;
 use solana_program::{
-    account_info::AccountInfo,
-    clock::Clock,
-    entrypoint::ProgramResult,
-    msg,
-    program::{invoke, invoke_signed},
-    program_error::ProgramError,
-    pubkey::Pubkey,
-    sysvar::Sysvar,
+    account_info::AccountInfo, clock::Clock, entrypoint::ProgramResult, msg,
+    program_error::ProgramError, pubkey::Pubkey, sysvar::Sysvar,
 };
-use spl_token_interface::instruction as token_instruction;
 
 use crate::{
     states::{Config, Vault, VAULT_SEED},
-    utils::{AccountCheck, ProcessInstruction, SignerAccount, WritableAccount},
+    utils::{
+        AccountCheck, AssociatedTokenAccount, AssociatedTokenAccountCheck, ConfigAccount, MintAccount, ProcessInstruction, SignerAccount, TokenProgram, TransferArgs, VaultAccount, WritableAccount
+    },
 };
 
 #[derive(Debug)]
@@ -26,6 +20,7 @@ pub struct BurnAndRefundV1Accounts<'a, 'info> {
     pub payer_ata: &'a AccountInfo<'info>,         // Owner's ATA
     pub vault_authority: &'a AccountInfo<'info>,   // Vault authority PDA
     pub config_pda: &'a AccountInfo<'info>,        // Config PDA
+    pub token_asset: &'a AccountInfo<'info>,       // Token mint
     pub token_program: &'a AccountInfo<'info>,     // Token program
 }
 
@@ -33,28 +28,45 @@ impl<'a, 'info> TryFrom<&'a [AccountInfo<'info>]> for BurnAndRefundV1Accounts<'a
     type Error = ProgramError;
 
     fn try_from(accounts: &'a [AccountInfo<'info>]) -> Result<Self, Self::Error> {
-        let [authority, nft_token_account, nft_mint, vault_pda, vault_ata, payer_ata, vault_authority, config_pda, token_program] =
-            accounts
+        let [
+            authority,
+            nft_token_account,
+            nft_asset,
+            vault_pda,
+            vault_ata,
+            payer_ata,
+            vault_authority,
+            config_pda,
+            token_mint,
+            token_program,
+        ] = accounts
         else {
             return Err(ProgramError::NotEnoughAccountKeys);
         };
 
         SignerAccount::check(authority)?;
         WritableAccount::check(nft_token_account)?;
+        AssociatedTokenAccount::check(nft_token_account, authority.key, nft_asset.key, token_program.key)?;
         WritableAccount::check(vault_pda)?;
+        VaultAccount::check(vault_pda)?;
         WritableAccount::check(vault_ata)?;
+        AssociatedTokenAccount::check(vault_ata, vault_pda.key, token_mint.key, token_program.key)?;
         WritableAccount::check(payer_ata)?;
+        AssociatedTokenAccount::check(payer_ata, authority.key, token_mint.key, token_program.key)?;
         WritableAccount::check(config_pda)?;
+        ConfigAccount::check(config_pda)?;
+        MintAccount::check(token_mint)?;
 
         Ok(Self {
             authority,
             nft_token_account,
-            nft_mint,
+            nft_mint: nft_asset,
             vault_pda,
             vault_ata,
             payer_ata,
             vault_authority,
             config_pda,
+            token_asset: token_mint,
             token_program,
         })
     }
@@ -64,6 +76,77 @@ impl<'a, 'info> TryFrom<&'a [AccountInfo<'info>]> for BurnAndRefundV1Accounts<'a
 pub struct BurnAndRefundV1<'a, 'info> {
     pub accounts: BurnAndRefundV1Accounts<'a, 'info>,
     pub program_id: &'a Pubkey,
+}
+
+impl<'a, 'info> BurnAndRefundV1<'a, 'info> {
+    fn check_vesting(&self, config: &Config, vault: &Vault) -> ProgramResult {
+        let authority = self.accounts.authority;
+
+        let clock = Clock::get()?;
+        if clock.unix_timestamp < config.vesting_end_ts {
+            msg!("Vesting no finished");
+            return Err(ProgramError::Custom(4));
+        }
+
+        if vault.is_unlocked() {
+            msg!("Vault already refunded");
+            return Err(ProgramError::Custom(5));
+        }
+
+        if vault.owner != *authority.key {
+            msg!("Vault owner mismatch");
+            return Err(ProgramError::Custom(6));
+        }
+
+        Ok(())
+    }
+
+    fn burn_nft(&self, config: &Config) -> ProgramResult {
+        let authority = self.accounts.authority;
+        let nft_token_account = self.accounts.nft_token_account;
+        let nft_mint = self.accounts.nft_mint;
+        let token_program = self.accounts.token_program;
+
+        TokenProgram::burn_nft(token_program, nft_token_account, nft_mint, authority, config.mint_decimals, &[])?;
+
+        Ok(())
+    }
+
+    fn refund_token(&self, config: &Config, vault: &mut Vault) -> ProgramResult {
+        let vault_ata = self.accounts.vault_ata;
+        let payer_ata = self.accounts.payer_ata;
+        let vault_authority = self.accounts.vault_authority;
+        let config_pda = self.accounts.config_pda;
+        let token_mint = self.accounts.token_asset;
+        let token_program = self.accounts.token_program;
+
+        let (expected_vault_auth, vault_bump) =
+            Pubkey::find_program_address(&[VAULT_SEED, config_pda.key.as_ref()], self.program_id);
+        if expected_vault_auth != *vault_authority.key {
+            msg!("Vault authority PDA mismatch");
+            return Err(ProgramError::InvalidAccountData);
+        }
+
+        let signers_seeds: &[&[&[u8]]] = &[&[VAULT_SEED, config_pda.key.as_ref(), &[vault_bump]]];
+
+        TokenProgram::transfer_signed(
+            TransferArgs {
+                source: vault_ata,
+                destination: payer_ata,
+                authority: vault_authority,
+                mint: token_mint,
+                token_program,
+                signer_pubkeys: &[],
+                amount: vault.amount,
+                decimals: config.mint_decimals,
+            },
+            signers_seeds,
+        )?;
+
+        vault.set_unlocked(true);
+
+        Ok(())
+    }
 }
 
 impl<'a, 'info> TryFrom<(&'a [AccountInfo<'info>], &'a Pubkey)> for BurnAndRefundV1<'a, 'info> {
@@ -83,92 +166,21 @@ impl<'a, 'info> TryFrom<(&'a [AccountInfo<'info>], &'a Pubkey)> for BurnAndRefun
 
 impl<'a, 'info> ProcessInstruction for BurnAndRefundV1<'a, 'info> {
     fn process(self) -> ProgramResult {
-        let authority = self.accounts.authority;
-        let nft_token_account = self.accounts.nft_token_account;
-        let nft_mint = self.accounts.nft_mint;
-        let vault_pda = self.accounts.vault_pda;
-        let vault_ata = self.accounts.vault_ata;
-        let payer_ata = self.accounts.payer_ata;
-        let vault_authority = self.accounts.vault_authority;
-        let config_pda = self.accounts.config_pda;
-        let token_program = self.accounts.token_program;
+        let config_data = self.accounts.config_pda.data.borrow_mut();
+        let config = Config::load(&config_data)?;
 
-        // Load config and vault
-        let cfg = Config::load(&config_pda.data.borrow())?;
-        let mut vault = Vault::load(&vault_pda.data.borrow())?;
+        let mut vault_data = self.accounts.vault_pda.data.borrow_mut();
+        let mut vault = Vault::load_mut(&mut vault_data)?;
 
-        // Vesting check
-        let clock = Clock::get()?;
-        if clock.unix_timestamp < cfg.vesting_end_ts {
-            msg!("Vesting no finished");
-            return Err(ProgramError::Custom(4));
-        }
+        self.check_vesting(&config, &vault)?;
 
-        if vault.is_unlocked {
-            msg!("Vault already refunded");
-            return Err(ProgramError::Custom(5));
-        }
+        self.burn_nft(&config)?;
 
-        if vault.owner != *authority.key {
-            msg!("Vault owner mismatch");
-            return Err(ProgramError::Custom(6));
-        }
+        self.refund_token(&config, &mut vault)?;
 
-        // Burn NFT (authority signs)
-        let burn_ix = token_instruction::burn(
-            token_program.key,
-            nft_token_account.key,
-            nft_mint.key,
-            authority.key,
-            &[],
-            1,
-        )?;
+        let amount = vault.amount;
 
-        invoke(
-            &burn_ix,
-            &[
-                nft_token_account.clone(),
-                nft_mint.clone(),
-                authority.clone(),
-                token_program.clone(),
-            ],
-        )?;
-
-        // Refund from vault to payer
-        let (expected_vault_auth, vault_bump) =
-            Pubkey::find_program_address(&[VAULT_SEED, config_pda.key.as_ref()], self.program_id);
-        if expected_vault_auth != *vault_authority.key {
-            msg!("Vault authority PDA mismatch");
-            return Err(ProgramError::InvalidAccountData);
-        }
-
-        let refund_ix = token_instruction::transfer(
-            token_program.key,
-            vault_ata.key,
-            payer_ata.key,
-            vault_authority.key,
-            &[],
-            vault.amount,
-        )?;
-
-        invoke_signed(
-            &refund_ix,
-            &[
-                vault_ata.clone(),
-                payer_ata.clone(),
-                vault_authority.clone(),
-                token_program.clone(),
-            ],
-            &[&[VAULT_SEED, config_pda.key.as_ref(), &[vault_bump]]],
-        )?;
-
-        vault.is_unlocked = true;
-        vault.serialize(&mut &mut vault_pda.data.borrow_mut()[..])?;
-
-        msg!(
-            "BurnAndRefund: burned NFT and refunded {} tokens",
-            vault.amount
-        );
+        msg!("BurnAndRefund: burned NFT and refunded {} tokens", amount);
 
         Ok(())
     }
