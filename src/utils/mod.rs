8f74866051a1a5520fd1d8f53f4491daa4a038ec
98prabowo <dimasprabowo98@icloud.com>
2025-11-01 03:52:20 +0700

    fix(utils): remove unused helpers

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,12 +1,7 @@
 mod account_check;
 mod merkle_proof;
-mod optional_account;
 mod process;
 
 pub use account_check::*;
 pub use merkle_proof::*;
-pub use optional_account::*;
 pub use process::*;
-
-#[cfg(test)]
-pub mod test_utils;
