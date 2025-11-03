8fd28ddc3e10b8c585585b5307c3c3b907b9e805
98prabowo <dimasprabowo98@icloud.com>
2025-11-03 17:48:14 +0700

    fix(states): remove unused authority PDA

--- src/states/authorities.rs
+++ src/states/authorities.rs
@@ -4,10 +4,3 @@ pub struct NftAuthority;
 impl NftAuthority {
     pub const SEED: &[u8; 13] = b"nft_authority";
 }
-
-#[derive(Debug)]
-pub struct VaultAuthority;
-
-impl VaultAuthority {
-    pub const SEED: &[u8; 15] = b"vault_authority";
-}
